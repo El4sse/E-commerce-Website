1dd5209045d83d914630c6bbb2e05050331261a4
Elasse <haouazimilyasse8@gmail.com>
2024-12-11 20:28:09 +0100

    Complete Home page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
-  return <div>Collection</div>;
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Fliter Options */}
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+        </p>
+
+        {/* Category Filter */}
+        <div className={`border border-gray-300 pl-5 mt-6`}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Collection;
