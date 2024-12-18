15076f7a615718000ba90e68feefd0a4437a531b
Elasse <haouazimilyasse8@gmail.com>
2024-12-18 13:19:54 +0100

    Create Product Model, User Model and connect API (login, register, admin)

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+// Route for user login
+const loginUser = async (req, res) => {
+    res.json({ msg: "Login API Working" });
+};
+
+// Route for user register
+const registerUser = async (req, res) => {
+    res.json({ msg: "Register API Working" });
+};
+
+// Route for admin login
+const adminLogin = async (req, res) => {
+    res.json({ msg: "Admin API Working" });
+};
+
+export { loginUser, registerUser, adminLogin };
